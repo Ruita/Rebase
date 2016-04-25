af81415985b9702ec52dd8fe1ea99c6af1b195ab
Ruita <ruita.ubale0@gmail.com>
2016-04-25 11:22:46 -0700

    -a

--- ExercisePartD/src/gitMerging/SomeClass.java
+++ ExercisePartD/src/gitMerging/SomeClass.java
@@ -1,13 +1,31 @@
 package gitMerging;
 
 public class SomeClass {
+
 	public void fnOne(){
+		System.out.println("On the other hand,you have different fingers");
+		
+	}
+	public void upcomingEvents(){
+		System.out.println("Upcoming Events");
+		System.out.println("Dinner at Katie's on Friday");
+
+	public void thoughtForTheDay(){
 		System.out.println("I couldn't repair your breakes, so I made your horn louder ");
 		
 	}
+	public void upcomingEvents(){
+	    System.out.println("Upcoming Events, Please join us at 6:00 PM");
+		System.out.println("Party at Jane's house tomorrow ");
+		System.out.println("All friends are comming");
+		
+
+	}
 	public static void main(String[] args)
 	{
-		
+		SomeClass sc=new SomeClass();
+		sc.thoughtForTheDay();
+		sc.upcomingEvents();
 	}
 
 }
